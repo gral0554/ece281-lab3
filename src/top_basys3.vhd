6e734a9e88d640f1d23db744afbeb2904c06f08a
gral0554 <olivedog01@outlook.com>
2025-04-03 20:51:18 -0600

    lab3

--- src/top_basys3.vhd
+++ src/top_basys3.vhd
@@ -133,9 +133,7 @@ begin
 	
 	-- CONCURRENT STATEMENTS ----------------------------
 	led(15 downto 13) <= light_L; -- LC, LB, LA
-    led(2) <= light_R(0);  -- RC
-    led(1) <= light_R(1);  -- RB
-    led(0) <= light_R(2);  -- RA
+    led(2 downto 0) <= light_R;
 	-- ground unused LEDs
 	-- leave unused switches UNCONNECTED
 	
